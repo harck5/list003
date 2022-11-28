4a3689ca49698499ad60788ff588cc210135a2d9
harck5 <raaaaullopez@gmail.com>
2022-11-28 11:34:08 +0100

    vb

--- Assets/Scripts/Exercise001.cs
+++ Assets/Scripts/Exercise001.cs
@@ -7,6 +7,7 @@ public class Exercise001 : MonoBehaviour
         public string letterX;
     void Start()
     {
+<<<<<<< Updated upstream
             letterC(letterX);
     }
     private bool letterC(string letter)
@@ -14,11 +15,25 @@ public class Exercise001 : MonoBehaviour
         if (letter == "a" || letter == "e" || letter == "i" || letter == "o" || letter == "u")
         {
             Debug.Log(message: $"{letter} is a vowel");
+=======
+        letterC();
+
+    }
+    private bool letterC()
+    {
+        if (letter == "a" || letter == "e" || letter == "i" || letter == "o" || letter == "u")
+        {
+            Debug.Log(message: $"{letter} is vowel");
+>>>>>>> Stashed changes
             return true;
         }
         else
         {
+<<<<<<< Updated upstream
             Debug.Log(message: $"{letter} is NOT a vowel");
+=======
+            Debug.Log(message: $"{letter} no es vocal");
+>>>>>>> Stashed changes
             return false;
         }
     }
